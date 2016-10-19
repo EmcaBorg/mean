3c9bac48fca19276520c588641d8a387b12d8f29
Emir Hadzimahmutovic <emcapentest@gmail.com>
2016-10-19 22:00:09 +0200

    Update login.component.ts

--- frontend_webpack/src/modules/core/typescript/components/login.component.ts
+++ frontend_webpack/src/modules/core/typescript/components/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'login',
-  template: '<p>Login</p>'
+  selector: 'universal',
+  template: '<p>Universal Component</p>'
 })
-export class LoginComponent {}
+export class UniversalComponent {}
