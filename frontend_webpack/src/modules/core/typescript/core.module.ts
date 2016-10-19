36d542248299b0366a707e2bca9c426538cd180e
Emir Hadzimahmutovic <emcapentest@gmail.com>
2016-10-19 22:00:28 +0200

    Update core.module.ts

--- frontend_webpack/src/modules/core/typescript/core.module.ts
+++ frontend_webpack/src/modules/core/typescript/core.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import {LoginComponent} from "./components/login.component";
 import {RestService} from "./services/rest.service";
 
 
